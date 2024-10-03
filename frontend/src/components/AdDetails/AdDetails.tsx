dc6e886d747be0c28638d609c1ccd9a021985907
PRISCILLA PAYET <priscilla.ppayet@gmail.com>
2024-10-03 10:34:39 +0200

    feature delete an ad ok

--- frontend/src/components/AdDetails/AdDetails.tsx
+++ frontend/src/components/AdDetails/AdDetails.tsx
@@ -1,14 +1,16 @@
 import { useEffect, useState } from 'react';
 import '../../index.css'
 
-import { useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import axios from 'axios';
 
 export type AdProps = {
+    created_at: Date;
     id:number;
     title: string;
     description: string;
     owner: string;
+    ownerEmail: string;
     picture: string;
     price: number;
     location: string
@@ -21,6 +23,7 @@ function AdDetails(){
 
     const {id} = useParams();
     const [ad, setAd] = useState<AdProps>();
+    const navigate = useNavigate(); // Hook pour la redirection
 
     useEffect (()=> {
         const fetchData = async () => {
@@ -35,24 +38,55 @@ function AdDetails(){
         fetchData();
     }, [id])
 
+    const handleDelete = async (id: number) => {
+        try {
+            const result = await axios.delete(`http://127.0.0.1:5000/ads/${id}`);
+            console.log("deleted",result);
+            alert("Annonce supprimée");
+            navigate("/");
+
+        } catch (err) {
+            console.log (err)
+        }
+    }
+
     return (
 
-        <div className='ad-details'>
-            {ad ?(
-                <>
-            <h2 className='ad-card-title'>{ad.title}</h2>
-            <div className='ad-details-image-container'>
-                <img src={ad.picture} alt={ad.title} className='ad-details-image'/></div>
-            <div className='ad-details-info'>
-                <p >{ad.description}</p>
-             <p className='ad-details-price'>{(ad.price/100).toFixed(2) }€</p>
-             </div>
-             </>
+     <> 
+        {ad ?(
+            <div>
+                <h2 className='ad-card-title'>{ad.title}</h2>
+                <section className="ad-details">
+                    <div className='ad-details-image-container'>
+                        <img src={ad.picture} alt={ad.title} className='ad-details-image'/>
+                    </div>
+                    <div className='ad-details-info'>
+                        <p className='ad-details-price'>{(ad.price/100).toFixed(2) }€</p>
+                        <p >{ad.description}</p>
+                        <hr className="separator" />
+                        <div className="ad-details-owner">
+                            <p>Annonce publiée par {ad.owner} le {new Date(ad.created_at).toLocaleDateString('fr-FR', { 
+                                                                                                                day: 'numeric', 
+                                                                                                                month: 'long', 
+                                                                                                                year: 'numeric' 
+                                                                                                                })}.</p>
+                        </div>
+                        <Link to={`mailto:${ad.ownerEmail}`} className="button button-primary link-button">
+                            Envoyer un email
+                        </Link>
+                        <button onClick={()=>handleDelete(ad.id)} className="button button-primary link-button">
+                            Supprimer cette annonce
+                    </button>
+                    </div>
+                    
+                    
+                    
+                </section>
+            </div>
         )
-        : (
-            <p>annonce non trouvée</p>
-        )}
-        </div>
+        : ( <p>annonce non trouvée</p>)
+        }
+    </>
       );
 }
 
